e4ea48fabc247cd46e8199d2bc0c7c61be5fec69
Naomi Hauret <naomi@troopers.email>
2019-06-02 16:17:32 +0200

    fixed wrong comment

---
+++ lambda/api/lessons/get.js
@@ -0,0 +1,31 @@
+//
+// Get all lessons from DB
+
+const app = require("./../../utils/app")
+const LessonModel = require("./../../modelslLesson")
+
+app
+  .get("/api/lessons", (req, res) => {
+    LessonModel.find({}).exec((err, data) => {
+      if (err) {
+        return res.send(500, err)
+      }
+      res.header("Content-Type", "application/json")
+      res.send(JSON.stringify(data))
+      res.end()
+    })
+  })
+  .get("/api/lessons/:uid", (req, res) => {
+    LessonModel.find({
+      lessonUID: req.params.uid,
+    }).exec((err, data) => {
+      if (err) {
+        return res.send(500, err)
+      }
+      res.header("Content-Type", "application/json")
+      res.send(JSON.stringify(data))
+      res.end()
+    })
+  })
+
+module.exports = app
